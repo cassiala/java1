18a3c452cc6445b84f7c49ce3330e001869d1ce9
cassiala <lais.lourenco.costa@escola.pr.gov.br>
2024-09-30 09:31:19 -0300

    \

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-function soma(num1,num2) {
-    return num1+num2;
-}
+// function soma(num1,num2) {
+//     return num1+num2;
+// }
 
-console.log(soma(2,3));
-function areaquadrado(lado){
-    return lado**2;
-}
-console.log(areaquadrado(3));
+// console.log(soma(2,3));
+// function areaquadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaquadrado(3));
 
-let areatriangulo=function(parâmetro){
-    resturn (2,3)/2;
-}
-console.log(areatriangulo(2,3));
+// let areatriangulo=function(parâmetro){
+//     resturn (2,3)/2;
+// }
+// console.log(areatriangulo(2,3));
 
-let nome=parseInt(prompt('seu numero?'));
-let nome2=parseInt(prompt('numero residencia?'));
-let nome3=parseInt(prompt('numero do responsavel?'));
-document.write()
-function nome(nome,nome2,nome3) {
-    return 
-}
+// let nome=parseInt(prompt('seu numero?'));
+// let nome2=parseInt(prompt('numero residencia?'));
+// let nome3=parseInt(prompt('numero do responsavel?'));
+// document.write()
+// function nome(nome,nome2,nome3) {
+//     return 
+// } 
+let estados =['Paraná','Maranhão','Piauí','Ceará','Rio Grande do Norte','Paraíba','Pernambuco','Alagoas','Sergipe','Recife','Roraima','Amazonas','Santa Catarina','Acre','Amapá','Espírito Santo','Rj','Sp','Pará','Rio Grande do Sul','Goías','Tocantins','Fortaleza','Mato Gorsso','Mato Grosso do Sul','Ceará','Distrito Federal']
